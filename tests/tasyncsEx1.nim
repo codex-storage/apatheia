e2fb9582d20278345a4eb5e7c24ccdeb07c239d8
Jaremy Creechley <creechley@gmail.com>
2024-02-08 22:18:20 -0700

    simple (wrong) example

--- tests/tasyncsEx1.nim
+++ tests/tasyncsEx1.nim
@@ -11,11 +11,23 @@ type
   ThreadArg = object
     startSig: ThreadSignalPtr
     doneSig: ThreadSignalPtr
-    value: int
+    value: float
+
+proc addNums(a, b: float, ret: ptr float) =
+  ret[] = a + b
 
 suite "async tests":
 
+  var tp = Taskpool.new(num_threads = 2) # Default to the number of hardware threads.
+
   asyncTest "test":
+    var args = ThreadArg()
+    args.startSig = ThreadSignalPtr.new().get()
+    args.doneSig = ThreadSignalPtr.new().get()
+
+    tp.spawn addNums(1, 2, addr args.value)
     await sleepAsync(100.milliseconds)
+    echo "\nRES: ", args.value
+
     check true
 
