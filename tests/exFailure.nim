6ddcc5d4f863fc3ed5c612ddf05aa83099e20139
Jaremy Creechley <creechley@gmail.com>
2024-02-21 19:51:43 -0700

    initial setup

---
+++ tests/exFailure.nim
@@ -0,0 +1,31 @@
+import std/os
+import std/sequtils
+
+import chronos
+import chronos/threadsync
+import chronos/unittest2/asynctests
+import taskpools
+
+import apatheia/queues
+
+proc worker(data: seq[char], queue: SignalQueue[int]) =
+  os.sleep(50)
+  echo "worker: ", data
+  discard queue.send(data.len())
+
+suite "async tests":
+
+  var tp = Taskpool.new(num_threads = 2) # Default to the number of hardware threads.
+  var queue = newSignalQueue[int]()
+
+  asyncTest "test":
+
+    ## init
+    var data = "hello world!".toSeq
+    tp.spawn worker(data, queue)
+
+    let res = await wait(queue).wait(1500.milliseconds)
+
+    check res.get() == 12
+
+
