7fcc8dba1bd064260f783d5bd6e3ad8eec56b9bd
Jaremy Creechley <creechley@gmail.com>
2024-02-21 23:00:03 -0700

    more examples

--- tests/exampleGcFailures/exFailureSeq.nim
+++ tests/exampleGcFailures/exFailureSeq.nim
@@ -24,7 +24,7 @@ type
 template toOpenArray*[T](arr: SeqDataPtr[T]): auto =
   system.toOpenArray(arr.data, 0, arr.size)
 
-proc toArrayHolder*[T](data: seq[T]): SeqDataPtr[T] =
+proc toSeqDataPtr*[T](data: seq[T]): SeqDataPtr[T] =
     SeqDataPtr[T](
       data: cast[ptr UncheckedArray[T]](unsafeAddr(data[0])), size: data.len()
     )
@@ -39,10 +39,10 @@ proc worker(data: SeqDataPtr[char], sig: ThreadSignalPtr) =
 
 proc runTest(tp: TaskPool, sig: ThreadSignalPtr) {.async.} =
   ## init
-  var obj = "hello world!".toSeqDataPtr()
+  var obj = "hello world!".toSeq()
 
   echo "spawn worker"
-  tp.spawn worker(obj.toArrayHolder(), sig)
+  tp.spawn worker(obj.toSeqDataPtr(), sig)
 
   ## adding fut.wait(100.milliseconds) creates memory issue
   await wait(sig).wait(10.milliseconds)
